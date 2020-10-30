5a8abc37b325c873093884659ca4a88529fac0b2
Michael Aldridge <maldridge@voiceops.com>
2020-10-29 22:40:54 -0700

    Make plugin debuggable

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
+	"context"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 
 	"github.com/resinstack/terraform-provider-linuxkit/linuxkit"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return linuxkit.Provider()
-		},
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "terraform.resinstack.io/resinstack/linuxkit",
+			&plugin.ServeOpts{ProviderFunc: linuxkit.Provider})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{ProviderFunc: linuxkit.Provider})
+	}
 }
